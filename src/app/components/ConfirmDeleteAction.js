2d8b4eed111624b34f679c8a996560f245898af4
Rauf <kurdrauf74@gmail.com>
2026-04-16 18:34:49 +0300

    feat: add delete confirmation using shadcn alert dialog

---
+++ src/app/components/ConfirmDeleteAction.js
@@ -0,0 +1,36 @@
+"use client";
+import { deletePost } from "../lib/actions/post";
+import { toast } from "sonner";
+import React, { useTransition } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+function ConfirmDeleteAction({ post }) {
+  const pathname = usePathname();
+  // to navigate page
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  async function handleDeletePost() {
+    startTransition(async () => {
+      const result = await deletePost(post?.id);
+      if (result?.success) {
+        toast.success(`${post?.title} deleted successfully!`);
+        router.push(`${pathname === "/" ? "/" : "/blogs"}`);
+      } else {
+        toast.error(`There was an error happend while deleting ${post.title}`);
+      }
+    });
+  }
+  return (
+    <Button
+      variant="destructive"
+      disabled={isPending}
+      onClick={handleDeletePost}
+    >
+      {" "}
+      {isPending ? "Deleting..." : "Yes, Delete"}
+    </Button>
+  );
+}
+
+export default ConfirmDeleteAction;
--- src/app/components/DeleteButton.js
+++ src/app/components/DeleteButton.js
@@ -1,42 +1,29 @@
 "use client";
 import { TrashIcon } from "lucide-react";
-import { deletePost } from "../lib/actions/post";
-import { toast } from "sonner";
-import { useTransition } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import React from "react";
 const styles = {
   primary:
     "bg-red-100 p-2 rounded-lg hover:opacity-80 hover:shadow-sm active:scale-103 hover:shadow-red-200 transition-all duration-200 cursor-pointer",
   secondary:
     "flex gap-2 items-center bg-red-100 px-4 py-2 rounded-lg hover:opacity-80 hover:shadow-sm active:scale-103 hover:shadow-red-200 transition-all duration-200 cursor-pointer",
 };
-function DeleteButton({ post, children, style }) {
-  const pathname = usePathname();
-  // to navigate page
-  const router = useRouter();
-  const [isPending, startTransition] = useTransition();
-  async function handleDeletePost() {
-    startTransition(async () => {
-      const result = await deletePost(post?.id);
-      if (result?.success) {
-        toast.success(`${post?.title} deleted successfully!`);
-        router.push(`${pathname === "/" ? "/" : "blogs"}`);
-      } else {
-        toast.error(`There was an error happend while deleting ${post.title}`);
-      }
-    });
-  }
+
+const DeleteButton = React.forwardRef(function DeleteButton(
+  { children, variant = "primary", ...props },
+  ref,
+) {
+  console.log(variant, styles[variant]);
   return (
     <button
-      disabled={isPending}
+      {...props}
       aria-label="delete post"
-      onClick={handleDeletePost}
-      className={`${isPending && "opacity-80 hover:shadow-none"} ${styles[style]}`}
+      ref={ref}
+      className={`${styles[variant]}`}
     >
       <TrashIcon size={18} className="text-red-500" />
       {children}
     </button>
   );
-}
+});
 
 export default DeleteButton;
