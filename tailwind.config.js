31894b31939c9e335c031fda1893586237120ed8
Rauf <kurdrauf74@gmail.com>
2026-04-16 18:35:12 +0300

    refactor: prefix shadcn theme variables with --color and --font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}", // Make sure this points to your UI components!
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
