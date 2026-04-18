3f898447df44658e2ccc68217646354b5be51e6d
Rauf <kurdrauf74@gmail.com>
2026-04-18 17:37:04 +0300

    refactor: move search and filter bar to standalone component

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -5,6 +5,7 @@ import ArticleList from "../components/ArticleList";
 import { Suspense } from "react";
 import Spinner from "../components/Spinner";
 import PostListSkeleton from "../components/skeletons/PostListSkeleton";
+import SearchFilterSort from "../components/SearchFilterSort";
 
 async function page({ searchParams }) {
   const params = await searchParams;
@@ -33,40 +34,7 @@ async function page({ searchParams }) {
           </div>
         </div>
         {/* search + filter */}
-        <div className="flex md:gap-x-10 md:gap-y-0 gap-y-4 flex-col md:flex-row">
-          <div className="flex gap-0.5 flex-col relative w-full">
-            <input
-              type="text"
-              id="search"
-              name="search"
-              className="border pl-8 w-full peer z-20 text-sm rounded-md px-3 py-2 focus:outline-none focus:ring-1 ring-indigo-500"
-              placeholder=" "
-            />
-            <label
-              htmlFor="search"
-              className="transition-all duration-200 absolute peer-[:not(:placeholder-shown)]:-top-0.5 peer-[:not(:placeholder-shown)]:left-2 peer-[:not(:placeholder-shown)]:text-xs peer-[:not(:placeholder-shown)]:bg-accent peer-[:not(:placeholder-shown)]:rounded-lg peer-[:not(:placeholder-shown)]:text-gray-50 peer-[:not(:placeholder-shown)]:px-1 peer-focus:px-1 peer-focus:-top-0.5 peer-focus:bg-accent peer-focus:text-xs z-40 peer-focus:rounded-lg peer-focus:text-gray-50 top-[50%] -translate-y-[50%] left-8 peer-focus:left-2 font-sora text-text-muted tracking-wide text-sm font-medium"
-            >
-              Search: Enter articles title..
-            </label>
-            <span className="absolute left-1 top-[50%] -translate-y-[50%] peer-focus:text-accent">
-              <Search />
-            </span>
-          </div>
-          <div className="relative w-full">
-            <select
-              name="cars"
-              id="cars"
-              className="border w-full border-black peer z-30 text-sm rounded-md text-text-muted px-8 py-2 focus:outline-none focus:ring-1 ring-indigo-500"
-            >
-              <option defaultChecked>Choose a filter</option>
-              <option value="saab">Saab</option>
-              <option value="mercedes">Mercedes</option>
-              <option value="audi">Audi</option>
-            </select>
-            <Filter className="absolute top-[50%] -translate-y-[50%] left-2 peer-focus:text-accent" />
-          </div>
-        </div>
-
+        <SearchFilterSort />
         {/* article list */}
 
         <Suspense fallback={<PostListSkeleton />}>
