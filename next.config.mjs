624969658438bf781112a6c1efe63eccb67d443b
Rauf <kurdrauf74@gmail.com>
2026-05-15 18:48:33 +0300

    chore(next-config): raise serverActions bodySizeLimit for image uploads.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "4mb", // Increases the allowed payload size to 4 Megabytes
+    },
+  },
   serverExternalPackages: ["@prisma/client"],
 };
 
