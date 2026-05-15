4f31a93de52720e62432afa911bbea0fbd59491c
Rauf <kurdrauf74@gmail.com>
2026-05-15 18:45:52 +0300

    feat(schema): validate profile picture size and file types using zod

--- src/lib/utils/schema.js
+++ src/lib/utils/schema.js
@@ -12,21 +12,42 @@ export const postFormSchema = z.object({
   category: z.string().min(1, "Category is required"),
 });
 
+// Define acceptable file types and maximum size (4MB)
+const MAX_FILE_SIZE = 4 * 1024 * 1024; // 4 Megabytes in bytes
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
 // register form validation
-export const registerSchema = z
-  .object({
-    firstName: z.string().min(2, "First Name must be at least 2 characters."),
-    lastName: z.string().min(2, "Last Name must be at least 2 characters."),
-    email: z.string().email("Invalid email address"),
-    password: z.string().min(8, "Password must be at least 8 characters"),
-    confirmPassword: z
-      .string()
-      .min(8, "Password must be at least 8 characters"),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not mutch.",
-    path: ["confirmPassword"],
-  });
+export const registerSchema = z.object({
+  firstName: z.string().min(2, "First Name must be at least 2 characters."),
+  lastName: z.string().min(2, "Last Name must be at least 2 characters."),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+  confirmPassword: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not mutch.",
+      path: ["confirmPassword"],
+    }),
+  profilePicture: z
+    .any()
+    .refine(
+      (file) => !file || file.length === 0 || file[0]?.size <= MAX_FILE_SIZE,
+      "Max image size is 4MB.",
+    )
+    .refine(
+      (file) =>
+        !file ||
+        file.length === 0 ||
+        ACCEPTED_IMAGE_TYPES.includes(file[0]?.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported.",
+    ),
+});
 
 // sign form validation
 export const signInSchema = z.object({
