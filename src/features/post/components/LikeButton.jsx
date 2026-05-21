0116ad9b72be81d91b6d48873afb3f8e3560c3ac
Rauf <kurdrauf74@gmail.com>
2026-05-21 18:49:14 +0300

    feat(ui): handle multiple user clicks and prevent duplicate submissions with useTransition for like system.

--- src/features/post/components/LikeButton.jsx
+++ src/features/post/components/LikeButton.jsx
@@ -1,20 +1,28 @@
 "use client";
 import { likePost } from "@/lib/actions/post";
 import { ThumbsUp } from "lucide-react";
+import { useTransition } from "react";
 import { toast } from "sonner";
 
 function LikeButton({ userLike, totalLikes, user, post }) {
+  const [isPending, startTranstion] = useTransition();
   async function handleLikePost() {
     if (!user) {
       toast.error("Register to like and comment on posts.");
     } else {
-      await likePost(post.id, user?.id);
+      startTranstion(async () => {
+        await likePost(post.id, user?.id);
+      });
     }
   }
   return (
-    <button className="flex gap-1 items-center" onClick={handleLikePost}>
+    <button
+      disabled={isPending}
+      className="flex gap-1 items-center"
+      onClick={handleLikePost}
+    >
       <ThumbsUp
-        className={`${userLike ? "text-indigo-400" : "text-slate-500"} active:scale-105 transition-all duration-200`}
+        className={`${userLike ? "text-indigo-400" : "text-slate-500"} ${isPending && "opacity-75"} active:scale-105 transition-all duration-200`}
         size={20}
         fill={`${userLike ? "#6366f1" : "none"}`}
 
