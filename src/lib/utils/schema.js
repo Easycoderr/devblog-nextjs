37d9d0149e20de4537720fd1f612cd8415a8c95e
Rauf <kurdrauf74@gmail.com>
2026-06-01 23:21:46 +0300

    fix: fix password comparison.

--- src/lib/utils/schema.js
+++ src/lib/utils/schema.js
@@ -36,32 +36,34 @@ export const postFormSchema = z.object({
 });
 
 // register form validation
-export const registerSchema = z.object({
-  firstName: z.string().min(2, "First Name must be at least 2 characters."),
-  lastName: z.string().min(2, "Last Name must be at least 2 characters."),
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
-  confirmPassword: z
-    .string()
-    .min(8, "Password must be at least 8 characters")
-    .refine((data) => data.password === data.confirmPassword, {
-      message: "Passwords do not mutch.",
-      path: ["confirmPassword"],
-    }),
-  profilePicture: z
-    .any()
-    .refine(
-      (file) => !file || file.length === 0 || file[0]?.size <= MAX_FILE_SIZE,
-      "Max image size is 4MB.",
-    )
-    .refine(
-      (file) =>
-        !file ||
-        file.length === 0 ||
-        ACCEPTED_IMAGE_TYPES.includes(file[0]?.type),
-      "Only .jpg, .jpeg, .png and .webp formats are supported.",
-    ),
-});
+export const registerSchema = z
+  .object({
+    firstName: z.string().min(2, "First Name must be at least 2 characters."),
+    lastName: z.string().min(2, "Last Name must be at least 2 characters."),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+    confirmPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters"),
+
+    profilePicture: z
+      .any()
+      .refine(
+        (file) => !file || file.length === 0 || file[0]?.size <= MAX_FILE_SIZE,
+        "Max image size is 4MB.",
+      )
+      .refine(
+        (file) =>
+          !file ||
+          file.length === 0 ||
+          ACCEPTED_IMAGE_TYPES.includes(file[0]?.type),
+        "Only .jpg, .jpeg, .png and .webp formats are supported.",
+      ),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not mutch.",
+    path: ["confirmPassword"],
+  });
 
 // sign form validation
 export const signInSchema = z.object({
