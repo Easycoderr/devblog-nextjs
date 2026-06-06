df89b695991c104693e8dcdcc40d148734011d82
Rauf <kurdrauf74@gmail.com>
2026-06-06 22:07:50 +0300

    feat: add a server action to fetch all user posts.

---
+++ src/lib/actions/profile/getPostsByUserId.js
@@ -0,0 +1,34 @@
+"use server";
+import getCurrentUser from "@/lib/getUser";
+import { prisma } from "@/lib/prisma";
+const POSTS_PER_PAGE = 8;
+async function getPostsByUserId(userId, currPage) {
+  if (!userId) return [];
+  const skip = (currPage - 1) * POSTS_PER_PAGE;
+  const user = await getCurrentUser();
+  try {
+    const [posts, totalCount] = await prisma.$transaction([
+      prisma.post.findMany({
+        skip: skip,
+        take: POSTS_PER_PAGE,
+        where: { authorId: userId },
+        include: {
+          ...(user
+            ? {
+                savedPosts: {
+                  where: { userId: user.id },
+                  select: { id: true },
+                },
+              }
+            : {}),
+        },
+      }),
+      prisma.post.count({ where: { authorId: userId } }),
+    ]);
+    return { posts, totalCount: Math.ceil(totalCount / POSTS_PER_PAGE) };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default getPostsByUserId;
