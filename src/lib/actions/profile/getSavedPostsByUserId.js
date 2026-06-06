8156895bcfdccf5ed9847a2211890f6ffffe01b0
Rauf <kurdrauf74@gmail.com>
2026-06-06 22:08:21 +0300

    feat: add a server action to fetch saved posts that user saved.

---
+++ src/lib/actions/profile/getSavedPostsByUserId.js
@@ -0,0 +1,39 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+const POSTS_PER_PAGE = 8;
+async function getSavedPostsByUserId(userId, currPage) {
+  const skip = (currPage - 1) * POSTS_PER_PAGE;
+  if (!userId) return [];
+  try {
+    const [savedPosts, totalCount] = await prisma.$transaction([
+      prisma.post.findMany({
+        skip: skip,
+        take: POSTS_PER_PAGE,
+        where: {
+          savedPosts: {
+            some: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc", // Shows the newest posts first
+        },
+      }),
+      prisma.post.count({
+        where: {
+          savedPosts: {
+            some: {
+              userId,
+            },
+          },
+        },
+      }),
+    ]);
+    return { savedPosts, totalCount: Math.ceil(totalCount / POSTS_PER_PAGE) };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default getSavedPostsByUserId;
