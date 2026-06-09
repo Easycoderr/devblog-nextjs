d3c061b59bba00ad4e8320868d74380935e1172e
Rauf <kurdrauf74@gmail.com>
2026-06-09 19:14:31 +0300

    fix: add condition if author is not the current user doesn't show the form.

--- src/app/blogs/edit/[id]/page.jsx
+++ src/app/blogs/edit/[id]/page.jsx
@@ -1,5 +1,6 @@
 import Form from "@/features/post/components/Form";
 import { getPost } from "@/lib/actions/post";
+import getCurrentUser from "@/lib/getUser";
 import { notFound } from "next/navigation";
 export async function generateMetadata({ params }) {
   const { id } = await params;
@@ -11,9 +12,17 @@ export async function generateMetadata({ params }) {
 }
 async function page({ params }) {
   const { id } = await params;
-  const post = await getPost(id);
+  const [post, user] = await Promise.all([getPost(id), getCurrentUser()]);
   if (!post) {
     notFound();
+  }
+
+  if (post.authorId !== user?.id) {
+    return (
+      <div className="bg-destructive/10 text-destructive text-lg inline-block mx-auto my-auto px-4 py-2 rounded-lg">
+        Access Denied. You do not own this post !
+      </div>
+    );
   }
   return (
     <div className="conatainer w-full mx-auto px-2 2xl:px-10 flex gap-8 flex-col items-center py-12">
