084559e253b3ce3e5f49a94700214cc90f6cf0d6
Rauf <kurdrauf74@gmail.com>
2026-06-21 18:51:56 +0300

    feat: add accent colors.

---
+++ src/config/accent-colors.js
@@ -0,0 +1,115 @@
+export const accentColors = [
+  "indigo",
+  "blue",
+  "emerald",
+  "purple",
+  "rose",
+  "amber",
+  "cyan",
+  "teal",
+];
+export const accentThemes = {
+  indigo: {
+    light: {
+      primary: "#6366f1",
+      hover: "#4f46e5",
+      ring: "#6366f1",
+    },
+    dark: {
+      primary: "#818cf8",
+      hover: "#6366f1",
+      ring: "#818cf8",
+    },
+  },
+
+  blue: {
+    light: {
+      primary: "#3b82f6",
+      hover: "#2563eb",
+      ring: "#3b82f6",
+    },
+    dark: {
+      primary: "#60a5fa",
+      hover: "#3b82f6",
+      ring: "#60a5fa",
+    },
+  },
+
+  emerald: {
+    light: {
+      primary: "#10b981",
+      hover: "#059669",
+      ring: "#10b981",
+    },
+    dark: {
+      primary: "#34d399",
+      hover: "#10b981",
+      ring: "#34d399",
+    },
+  },
+
+  purple: {
+    light: {
+      primary: "#8b5cf6",
+      hover: "#7c3aed",
+      ring: "#8b5cf6",
+    },
+    dark: {
+      primary: "#a78bfa",
+      hover: "#8b5cf6",
+      ring: "#a78bfa",
+    },
+  },
+
+  rose: {
+    light: {
+      primary: "#f43f5e",
+      hover: "#e11d48",
+      ring: "#f43f5e",
+    },
+    dark: {
+      primary: "#fb7185",
+      hover: "#f43f5e",
+      ring: "#fb7185",
+    },
+  },
+
+  amber: {
+    light: {
+      primary: "#f59e0b",
+      hover: "#d97706",
+      ring: "#f59e0b",
+    },
+    dark: {
+      primary: "#fbbf24",
+      hover: "#f59e0b",
+      ring: "#fbbf24",
+    },
+  },
+
+  cyan: {
+    light: {
+      primary: "#06b6d4",
+      hover: "#0891b2",
+      ring: "#06b6d4",
+    },
+    dark: {
+      primary: "#22d3ee",
+      hover: "#06b6d4",
+      ring: "#22d3ee",
+    },
+  },
+
+  teal: {
+    light: {
+      primary: "#14b8a6",
+      hover: "#0f766e",
+      ring: "#14b8a6",
+    },
+    dark: {
+      primary: "#2dd4bf",
+      hover: "#14b8a6",
+      ring: "#2dd4bf",
+    },
+  },
+};
