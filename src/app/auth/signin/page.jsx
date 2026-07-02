771e7c3bde6e5685cbfd65262899c0c76070be45
Rauf <kurdrauf74@gmail.com>
2026-07-02 18:43:01 +0300

    refactor: wrap VerificationToast in Suspense and remove unused searchParams.

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -1,14 +1,12 @@
 import AuthSigninForm from "@/features/auth/components/AuthSigninForm";
 import VerificationToast from "@/features/auth/components/VerificationToast";
-import { toast } from "sonner";
-
-async function page({ searchParams }) {
-  const { verified } = await searchParams;
-  if (verified) {
-    <VerificationToast />;
-  }
+import { Suspense } from "react";
+async function page() {
   return (
     <div className="conatainer mx-auto px-2 flex gap-8 flex-col items-center justify-center py-12">
+      <Suspense fallback={null}>
+        <VerificationToast />
+      </Suspense>
       <AuthSigninForm />
     </div>
   );
