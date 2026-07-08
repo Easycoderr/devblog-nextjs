606f4c08fe628c22fd6a1d11fe71203d14bd0ec6
Rauf <kurdrauf74@gmail.com>
2026-07-08 17:39:19 +0300

    refactor: replace manual verefication failed message with VereficationFailed component.

--- src/app/auth/verify-email/page.jsx
+++ src/app/auth/verify-email/page.jsx
@@ -1,3 +1,4 @@
+import VerificationFailed from "@/features/auth/components/VerificationFailed";
 import verifyToken from "@/lib/actions/verify-email/verifyToken";
 import { AlertCircle, ArrowLeft } from "lucide-react";
 import Link from "next/link";
@@ -8,27 +9,7 @@ async function page({ searchParams }) {
   if (!verificationToken.success) {
     return (
       <div className="w-full min-h-[600px] flex items-center justify-center">
-        <div className="flex flex-col gap-4 items-center bg-card text-card-foreground shadow-md p-8 rounded-xl max-w-md text-center">
-          <AlertCircle className="size-14 text-brand-danger" />
-
-          <div className="space-y-1">
-            <h2 className="text-xl font-semibold tracking-tight">
-              Verification Failed
-            </h2>
-            <p className="text-muted-foreground text-base">
-              {verificationToken?.message ||
-                "Your verification link has expired or is invalid."}
-            </p>
-          </div>
-
-          <Link
-            href="/auth/signin"
-            className="flex gap-2 text-white bg-brand-danger hover:bg-brand-danger/90 transition-colors rounded-lg px-4 py-2 mt-2 items-center tracking-wide font-medium"
-          >
-            <ArrowLeft className="size-4" />
-            <span>Back to Sign In</span>
-          </Link>
-        </div>
+        <VerificationFailed message={verificationToken.message} />
       </div>
     );
   }
