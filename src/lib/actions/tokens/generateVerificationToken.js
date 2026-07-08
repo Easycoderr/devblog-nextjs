b2718da84425afe28250a461967cdc5fd3e788e3
Rauf <kurdrauf74@gmail.com>
2026-07-08 15:48:34 +0300

    feat: add dedicated file for generating verification token.

---
+++ src/lib/actions/tokens/generateVerificationToken.js
@@ -0,0 +1,20 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+import crypto from "crypto";
+async function generateVerificationToken(email) {
+  try {
+    const token = crypto.randomBytes(32).toString("hex");
+    await prisma.verificationToken.create({
+      data: {
+        identifier: email,
+        token: token,
+        expires: new Date(Date.now() + 1000 * 60 * 10), // 10 Min
+      },
+    });
+    return token;
+  } catch (error) {
+    console.log("Something went wrong while generate token, ERROR:", error);
+  }
+}
+
+export default generateVerificationToken;
