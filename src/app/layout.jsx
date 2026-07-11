2ec915a9a72be6d0a5d519bb69803d7d0c67733d
Rauf <kurdrauf74@gmail.com>
2026-07-11 14:53:41 +0300

    refactor: rename middleware file to proxy, and install latest versions of tailwind and prostcss.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,4 @@
-import { Inter, Sora, Space_Grotesk, JetBrains_Mono } from "next/font/google";
-
+import { Inter, Sora, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
