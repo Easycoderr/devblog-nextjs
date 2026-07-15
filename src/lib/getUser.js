617234c87a3e85914a618a1b84e4486ac9286eb3
Rauf <kurdrauf74@gmail.com>
2026-07-15 21:46:45 +0300

    feat: add bio column to getCurrentUser server action.

--- src/lib/getUser.js
+++ src/lib/getUser.js
@@ -15,6 +15,7 @@ async function getCurrentUser() {
         lastName: true,
         provider: true,
         avatar: true,
+        bio: true,
       },
     });
     return { userName, email, name, ...user } ?? null;
