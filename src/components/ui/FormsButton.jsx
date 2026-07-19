1a1c2773faff66b6d352db86f391d34cc7f2ffa4
Rauf <kurdrauf74@gmail.com>
2026-07-19 15:18:55 +0300

    fix: remove hardcoded tailwindcss class.

--- src/components/ui/FormsButton.jsx
+++ src/components/ui/FormsButton.jsx
@@ -11,11 +11,31 @@ function FormsButton({
   style,
   onClick,
 }) {
+  console.log("DISABLED:", disabled);
   const styles = {
-    authForm: `${disabled ? "bg-primary/75 cursor-not-allowed" : "cursor-pointer bg-primary hover:opacity-75 active:scale-103"} px-4 py-2 tracking-wider border text-indigo-50 shadow rounded-lg text-nowrap transition-all duration-200`,
-    form: `${disabled ? "bg-primary/75 cursor-not-allowed" : "cursor-pointer bg-primary hover:opacity-75 active:scale-103"} px-2 py-1 tracking-wider border text-indigo-50 shadow rounded-lg text-nowrap transition-all duration-200`,
-    settingsPrimary: `${disabled ? "bg-opacity-75 cursor-not-allowed" : "cursor-pointer"} text-primary inline-block w-full bg-primary/20 rounded-lg py-2 px-4 hover:opacity-75 cursor-pointer text-nowrap transition-all duration-200`,
-    settingsSecondary: `${disabled ? "bg-opacity-75 cursor-not-allowed" : "cursor-pointer"} text-destructive inline-block w-full bg-destructive/20 rounded-lg py-2 px-4 hover:opacity-75 text-nowrap transition-all duration-200`,
+    authForm: `${
+      disabled
+        ? "bg-primary/75 cursor-not-allowed"
+        : "cursor-pointer bg-primary hover:opacity-75 active:scale-103"
+    } px-4 py-2 tracking-wider border text-indigo-50 shadow rounded-lg text-nowrap transition-all duration-200`,
+
+    form: `${
+      disabled
+        ? "bg-primary/75 cursor-not-allowed"
+        : "cursor-pointer bg-primary hover:opacity-75 active:scale-103"
+    } px-2 py-1 tracking-wider border text-indigo-50 shadow rounded-lg text-nowrap transition-all duration-200`,
+
+    settingsPrimary: `${
+      disabled
+        ? "opacity-50 cursor-not-allowed"
+        : "cursor-pointer hover:opacity-75"
+    } text-primary inline-block w-full bg-primary/20 rounded-lg py-2 px-4 text-nowrap transition-all duration-200`,
+
+    settingsSecondary: `${
+      disabled
+        ? "opacity-50 cursor-not-allowed"
+        : "cursor-pointer hover:opacity-75"
+    } text-destructive inline-block w-full bg-destructive/20 rounded-lg py-2 px-4 text-nowrap transition-all duration-200`,
   };
 
   return (
