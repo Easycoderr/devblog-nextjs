26f7b2cd47bf32904440ef8bdd27fa1e7c1af58c
Rauf <kurdrauf74@gmail.com>
2026-07-19 15:42:03 +0300

    feat: implement sendChangeEmail server action using Resend

---
+++ src/lib/actions/mail/sendChangeEmail.js
@@ -0,0 +1,28 @@
+"use server";
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
+async function sendChangeEmail(email, token) {
+  const verificationLink = `${process.env.AUTH_URL}/auth/change-email?token=${token}`;
+  try {
+    const response = await resend.emails.send({
+      from: "DevBlog <onboarding@resend.dev>", // change later
+      to: email,
+      subject: "Change email",
+      html: `
+      <h2>Welcome to DevBlog</h2>
+      <p>Change your email by clicking the button below.</p>
+
+      <a href="${verificationLink}">
+      Chnage email
+      </a>
+      `,
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Critical error while sending Change email link:", error);
+    return { success: false, message: "An unexpected error occurred." };
+  }
+}
+
+export default sendChangeEmail;
