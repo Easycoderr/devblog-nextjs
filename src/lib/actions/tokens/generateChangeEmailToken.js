2b292014599536e2fe1aed891979e989d2994378
Rauf <kurdrauf74@gmail.com>
2026-07-19 15:43:01 +0300

    feat: implement generateChangeEmailToken utility to create and store verification tokens.

---
+++ src/lib/actions/tokens/generateChangeEmailToken.js
@@ -0,0 +1,24 @@
+import { prisma } from "@/lib/prisma";
+import crypto from "crypto";
+
+async function generateChangeEmailToken(email, newEmail) {
+  const token = await crypto.randomBytes(32).toString("hex");
+  try {
+    await prisma.emailChangeToken.create({
+      data: {
+        identifier: email,
+        token: token,
+        newEmail,
+        expires: new Date(Date.now() + 1000 * 60 * 10), // 10 Min
+      },
+    });
+    return token;
+  } catch (error) {
+    console.log(
+      "Something went wrong while generate reset password token, ERROR:",
+      error,
+    );
+  }
+}
+
+export default generateChangeEmailToken;
