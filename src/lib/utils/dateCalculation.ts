bef7906b8dc2c84954cd1d866731bea993e47cae
Rauf <kurdrauf74@gmail.com>
2026-08-15 15:37:36 +0300

    refactor(ts): convert dateCalculation to TypeScript.

--- src/lib/utils/dateCalculation.ts
+++ src/lib/utils/dateCalculation.ts
@@ -1,8 +1,7 @@
-import { format, formatDistance, isValid, subDays } from "date-fns";
-function dateCalculation(date, type) {
+import { format, formatDistance } from "date-fns";
+function dateCalculation(date: Date, type: string): string {
   const newDate = new Date(date);
-  const isValidDate = date && !isNaN(newDate.getTime());
-
+  const isValidDate = !isNaN(newDate.getTime());
   if (isValidDate && type === "full") {
     return format(newDate, "MMMM yyyy");
   }
