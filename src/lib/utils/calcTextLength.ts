cca0dffaceca2c30180191bec556e90dd0da4da2
Rauf <kurdrauf74@gmail.com>
2026-08-15 15:30:28 +0300

    refactor(ts): convert calcTextRange to TypeScript.

--- src/lib/utils/calcTextLength.ts
+++ src/lib/utils/calcTextLength.ts
@@ -1,4 +1,4 @@
-function calcTextRange(text = [], maxLength) {
+function calcTextRange(text: string[] = [], maxLength: number): string {
   const textLength = text.length;
   return `(${textLength}/${maxLength})`;
 }
