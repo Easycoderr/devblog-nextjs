0a9bb657b325f6728fa6e8e3ee775292b916b5ff
Rauf <kurdrauf74@gmail.com>
2026-08-23 18:47:48 +0300

    refactor(ts): migrate updatePassword server actions to TypeScript.

--- src/lib/actions/settings/account/updatePassword.ts
+++ src/lib/actions/settings/account/updatePassword.ts
@@ -2,27 +2,40 @@
 import getCurrentUser from "@/lib/getUser";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
-async function updatePassword(data) {
+
+type UpdatePassword = {
+  currentPassword: string;
+  newPassword: string;
+};
+
+async function updatePassword(data: UpdatePassword) {
   const currUser = await getCurrentUser();
+  if (!currUser) return null;
   const user = await prisma.user.findUnique({
     where: { id: currUser.id },
     select: { password: true },
   });
+  if (!user) return null;
+  if (!user.password)
+    return { error: true, message: "Something went wrong, try again!" };
   const isValid = await bcrypt.compare(data.currentPassword, user.password);
   if (!isValid) {
     return { error: true, message: "Incorrect password" };
   }
-  const hashedpassword = await bcrypt.hash(data.newPassword, 10);
+  const hashedPassword = await bcrypt.hash(data.newPassword, 10);
   try {
-    const result = await prisma.user.update({
+    await prisma.user.update({
       where: { id: currUser.id },
-      data: { password: hashedpassword },
+      data: { password: hashedPassword },
     });
-    console.log(result);
-    if (result)
-      return { error: false, message: "password changed successfully" };
+
+    return { error: false, message: "password changed successfully" };
   } catch (error) {
     console.log("Something went wrong while change password:", error);
+    return {
+      error: true,
+      message: "Something went wrong. Please try again.",
+    };
   }
 }
 
