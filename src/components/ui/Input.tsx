b78c74a629704261dd98570c35716b2e805cede1
Rauf <kurdrauf74@gmail.com>
2026-09-03 18:26:26 +0300

    refactor(ts): migrate input custom component to TypeScript.

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Eye, EyeClosed } from "lucide-react";
+import { type ReactNode, useState } from "react";
+import { FieldError } from "react-hook-form";
+type InputProps = React.ComponentProps<"input"> & {
+  label: string;
+  error?: FieldError;
+  icon?: ReactNode;
+  type?: string;
+};
+function Input({ label, error, icon, type, ...props }: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="flex flex-col gap-1 w-full">
+      <label
+        htmlFor={label.split(" ").join("")}
+        className="text-muted-foreground font-semibold tracking-wide text-sm"
+      >
+        {label}
+      </label>
+      <div className="relative">
+        <input
+          id={label.split(" ").join("")}
+          type={
+            type ||
+            `${icon === "password" && !showPassword ? "password" : "text"}`
+          }
+          className={`${error ? "border-destructive focus:border-destructive" : "bg-input border-border focus:ring focus:ring-primary"} p-2.5 pr-8 border rounded-lg w-full text-sm focus:outline-none`}
+          {...props}
+        />
+        {icon !== "password" && (
+          <span className="absolute right-2 top-[50%] translate-y-[-50%]">
+            {icon}
+          </span>
+        )}
+        {icon === "password" && (
+          <button
+            type="button"
+            onClick={() => setShowPassword((show) => !show)}
+            className="absolute cursor-pointer  right-2 top-[50%] translate-y-[-50%]"
+          >
+            {showPassword ? (
+              <EyeClosed size={20} className="text-muted-foreground" />
+            ) : (
+              <Eye size={20} className="text-muted-foreground" />
+            )}
+          </button>
+        )}
+      </div>
+      <span className="flex">
+        {error && (
+          <p className="text-destructive bg-destructive/10 px-2 py-1 rounded-md text-xs">
+            {error.message}
+          </p>
+        )}
+      </span>
+    </div>
+  );
+}
+
+export default Input;
