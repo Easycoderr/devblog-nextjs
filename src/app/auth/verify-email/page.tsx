a974cb78d37755d2b12a891c62bed3830e786817
Rauf <kurdrauf74@gmail.com>
2026-09-13 17:16:19 +0300

    refactor(ts): migrate sign in and verifyEmail pages to TypeScript.

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -1,9 +1,16 @@
 import VerificationFailed from "@/features/auth/components/VerificationFailed";
 import verifyToken from "@/lib/actions/verify/verifyToken";
 
-async function page({ searchParams }) {
-  const { token } = await searchParams;
+async function page({ searchParams }: { searchParams: { token: string } }) {
+  const { token } = searchParams;
   const verificationToken = await verifyToken(token);
+  if (!verificationToken) {
+    return (
+      <div className="w-full min-h-150 flex items-center justify-center">
+        <VerificationFailed message="Something went wrong failed to verify Email, please try again!" />
+      </div>
+    );
+  }
   if (!verificationToken.success) {
     return (
       <div className="w-full min-h-150 flex items-center justify-center">
