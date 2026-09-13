f73ac3718330383fae7fba0fb50fb00a34094ee9
Rauf <kurdrauf74@gmail.com>
2026-09-13 17:11:50 +0300

    refactor(ts): migrate forget password and reset password and register pages to TypeScript.

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,11 +1,13 @@
+import ErrorCard from "@/components/ui/ErrorCard";
 import ResetPasswordForm from "@/features/auth/components/ResetPasswordForm";
 import VerificationFailed from "@/features/auth/components/VerificationFailed";
 import verifyResetPasswordToken from "@/lib/actions/verify/verifyResetPasswordToken";
 
-async function page({ searchParams }) {
-  const params = await searchParams;
-  const token = params.token;
+async function page({ searchParams }: { searchParams: { token: string } }) {
+  const { token } = searchParams;
+
   const response = await verifyResetPasswordToken(token);
+  if (!response) return null;
 
   return (
     <div className="flex items-center justify-center min-h-150">
