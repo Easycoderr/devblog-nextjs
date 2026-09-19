a684df6c0f7bd16af332b700f419ce657f976770
Rauf <kurdrauf74@gmail.com>
2026-09-19 16:48:07 +0300

    feat(action): implement logout all sessions server action..

---
+++ src/lib/actions/settings/security/logoutAllSessions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { signOut } from "next-auth/react";
+
+async function logoutAllSessions() {
+  const session = await auth();
+
+  if (!session?.user?.id) return { success: false, message: "Unauthorized" };
+
+  await prisma.session.deleteMany({ where: { userId: session.user.id } });
+
+  await signOut({ redirectTo: "/auth/signin" });
+}
+
+export default logoutAllSessions;
